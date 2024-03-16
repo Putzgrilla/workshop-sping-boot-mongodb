33a4cbef5b51d344e1ad5205a2af9bcfe08173f1
arthur <arthurmariano2003@gmail.com>
2024-03-16 16:52:30 -0300

    query2

--- src/main/java/com/Arthur/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/Arthur/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,15 @@ package com.Arthur.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.Arthur.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	@Query("{ 'title': { $regex:?0 /pattern/, $options: 'i' } }")
+	List<Post> find(String text);
 	List<Post> findByTitleContaining(String text);
 
 }
